d0f2a02ce9e8fdf54e67aee4fa6f2e9eff682e47
M-anan779 <manan.gupta060204@gmail.com>
2025-07-27 14:51:53 -0700

    feat(pipeline): add basic CLI to run the entire pipeline stage wise

---
+++ src/pipeline/run_pipeline.py
@@ -0,0 +1,41 @@
+from src.pipeline import data_loader as loader
+from src.pipeline import data_parser as parser
+from src.pipeline import data_aggregator as aggregator
+from src.pipeline import data_preprocessor as preprocessor
+
+
+def main():
+    exit = False
+
+    while (not exit):
+
+        print("\nSelect an option:\n")
+        print("1. Loader")
+        print("2. Parser")
+        print("3. Aggregator")
+        print("4. Preprocessor")
+        print("5. All")
+        print("6. Exit")
+
+        choice = input("> ")
+
+        if choice == '1':
+            loader.run()
+        elif choice == '2':
+            parser.run()
+        elif choice == '3':
+            aggregator.run()
+        elif choice == '4':
+            preprocessor.run()
+        elif choice == '5':
+            loader.run()
+            parser.run()
+            aggregator.run()
+            preprocessor.run()
+        elif choice == '6':
+            exit = True
+        else:
+            print("\nInvalid output...\n")
+
+if __name__ == "__main__":
+    main()
