87a36eb164877b0f7319b400073e6f94bcbd6d99
M-anan779 <manan.gupta060204@gmail.com>
2025-07-30 16:21:28 -0700

    dummy file to skirt not having to install python 3.13

---
+++ src/imghdr.py
@@ -0,0 +1,8 @@
+# imghdr.py - Minimal stub for Python 3.13 to keep TensorBoard working
+
+def what(file, h=None):
+    return None
+
+# TensorBoard expects this list for registering tests
+tests = []
+
