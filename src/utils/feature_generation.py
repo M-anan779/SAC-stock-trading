6fdc6dc178bf12f42f836955accb7b030caf1d52
M-anan779 <manan.gupta060204@gmail.com>
2025-09-29 21:34:53 -0700

    (Change): Added information term from Shannon Entropy as a feature, using the price z score

--- src/utils/feature_generation.py
+++ src/utils/feature_generation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pandas_ta as ta
+from scipy import special
 
 class TechnicalIndicators:
     def __init__(self, df):
@@ -16,7 +17,7 @@ class TechnicalIndicators:
 
         dmi_dev = dmp - dmn
         self.df["DMI_dev"] = dmi_dev
-        self.df["ADX"] = adx["ADX_14"]
+        self.df["ADX"] = (adx["ADX_14"] / 100).clip(0, 1)
     
     def _add_trend_confirmation_features(self):
         K_length = 10
@@ -31,12 +32,11 @@ class TechnicalIndicators:
         smooth_highlow_range = ta.ema(ta.ema(highlow_range, D_length), D_length)
         smi = 200 * (smooth_relative_range / (smooth_highlow_range.replace(0, 1e-8)))
 
-        self.df["time_since_reversal"] = self._time_since_reversal_helper(series=smi, upper_thresh=40, lower_thresh=-40)
-
         ema_length = 3
         smi_dev = smi - ta.ema(smi, ema_length)
         smi_dev_delta = smi_dev - smi_dev.shift(axis=0, periods=1).fillna(0)
         self.df["SMI_dev_delta"] = smi_dev_delta
+        self.df["time_since_reversal"] = (self._time_since_reversal_helper(series=smi, upper_thresh=40, lower_thresh=-40) / 390).clip(0, 1)
 
     @staticmethod
     def _time_since_reversal_helper(series, upper_thresh, lower_thresh):
@@ -52,44 +52,40 @@ class TechnicalIndicators:
         
         return pd.Series(result, index=series.index)
     
-    def _add_price_zscore(self):
+    # price z score + information content (used in Shannon Entropy)
+    def _add_price_features(self):
         rolling_mean = self.df["close"].rolling(30).mean()
         rolling_std = self.df["close"].rolling(30).std(ddof=0)
         self.df["price_zscore"] = ((self.df["close"] - rolling_mean) / rolling_std.replace(0, 1e-8)).clip(-5, 5)
 
-    def _add_candle_strength_features(self):
-        true_range = (self.df["high"] - self.df["low"]).replace(0, 1e-8)  # True range per bar
-        body = self.df["close"] - self.df["open"]
-
-        # candle body size normalized by range
-        self.df["price_movement"] = (body / true_range).clip(-5, 5)
+        # standard normal CDF
+        calculate_phi = lambda x: 0.5 * (1.0 + special.erf(x / np.sqrt(2.0)))
+        phi = 2.0 * (1.0 - calculate_phi(abs(self.df["price_zscore"])))                                         # two-sided tail prob calculated using
+        information = (-np.log(np.maximum(phi, 1e-8)) / 15).clip(-1, 1)                                                # information content, how "surprising" a price jump is
+        self.df["entropy"] = information
 
     def _add_volatility_zscore(self):
         returns = self.df["close"].pct_change()
         vol = returns.rolling(30).std()
-        rolling_mean = vol.rolling(60).mean()
-        rolling_std = vol.rolling(60).std(ddof=0)
+        rolling_mean = vol.rolling(30).mean()
+        rolling_std = vol.rolling(30).std(ddof=0)
         self.df["volatility_zscore"] = ((vol - rolling_mean) / rolling_std.replace(0, 1e-8)).clip(-5, 5)
 
+    # normalize z score columns through tanh
     def _normalize(self):
-        self.df["time_since_reversal"] = (self.df["time_since_reversal"] / 78).clip(0,1)
-        self.df["ADX"] = (self.df["ADX"] / 50)
-        
-        cols = ["DMI_dev", "SMI_dev_delta", "price_movement"]
+        cols = ["DMI_dev", "SMI_dev_delta"]
         rolling_mean = self.df[cols].shift(1).rolling(30).mean()
         rolling_std = self.df[cols].shift(1).rolling(30).std(ddof=0)
         self.df[cols] = ((self.df[cols] - rolling_mean) / rolling_std.replace(0, 1e-8)).clip(-5, 5)
-        
         cols.append("price_zscore")
         cols.append("volatility_zscore")
-        self.df[cols] = np.tanh(self.df[cols])
+        self.df[cols] = np.tanh(self.df[cols]).clip(-1, 1)
         
     def generate_all_indicators(self):
         self._add_market_price()
         self._add_directional_features()
         self._add_trend_confirmation_features()
-        self._add_price_zscore()
-        self._add_candle_strength_features()
+        self._add_price_features()
         self._add_volatility_zscore()
         self._normalize()
 
